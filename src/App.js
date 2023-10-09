af897ec91ad6dd748ba8f885477181da2fa2d223
HARSH SHARMA <116057247+harsSharma45@users.noreply.github.com>
2023-10-09 23:33:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { Explore, Liked, SearchFeed, SearchPage, VideoPlayer} from "./components";
+import { Explore, Liked, SearchFeed, SearchPage, VideoPlayer } from "./components";
 import './index.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from "react-redux";
 import store from "./Store/store";
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
+    // Wrap the entire application with the Redux Provider to provide the store to all components.
     <Provider store={store}>
       <BrowserRouter>
-      <ScrollToTop/>
+        {/* ScrollToTop component ensures that the page scrolls to the top when navigating to a new route. */}
+        <ScrollToTop />
         <Routes>
-          <Route path="/" element={<SearchPage/>} />
-          <Route path="/Search/:searchedTerm" element={<SearchFeed/>} />
-          <Route path="/Video/:videoId" element={<VideoPlayer/>} />
-          <Route path="/Explore" element={<Explore/>} />
-          <Route path="/Liked" element={<Liked/>} />
+          {/* Define different routes and their corresponding components. */}
+          <Route path="/" element={<SearchPage />} /> {/* Home page */}
+          <Route path="/Search/:searchedTerm" element={<SearchFeed />} /> {/* Search results */}
+          <Route path="/Video/:videoId" element={<VideoPlayer />} /> {/* Video player */}
+          <Route path="/Explore" element={<Explore />} /> {/* Explore page */}
+          <Route path="/Liked" element={<Liked />} /> {/* Liked videos */}
         </Routes>
       </BrowserRouter>
     </Provider>
