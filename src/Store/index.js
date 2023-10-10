eedc832caea960654ea794e7362cd58900244d35
HARSH SHARMA <116057247+harsSharma45@users.noreply.github.com>
2023-10-10 16:08:19 +0530

    Update index.js
    ## Changes Made
    
    - Added 'createSlice' function from '@reduxjs/toolkit' to create a Redux slice.
    - Created a 'darkMode' slice with an initial state of 'true', indicating dark mode is enabled by default.
    - Implemented a 'toggleDarkMode' reducer within the 'darkMode' slice, enabling the toggling of dark and light modes based on user interaction.
    - Exported the 'toggleDarkMode' action creator to dispatch the 'toggleDarkMode' action.
    - Exported the 'darkMode.reducer' to handle state updates based on dispatched actions.

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,16 +1,26 @@
-const { createSlice } = require('@reduxjs/toolkit')
+// Import the 'createSlice' function from '@reduxjs/toolkit'
+const { createSlice } = require('@reduxjs/toolkit');
 
+// Create a slice named 'darkMode' using 'createSlice'
 const darkMode = createSlice({
+    // Specify the name of the slice
     name: 'darkMode',
+    
+    // Initialize the initial state to 'true', indicating dark mode is initially enabled
     initialState: true,
-    reducers:{
-        toggleDarkMode(state, action){
-            return(
-                !state
-            )
+    
+    // Define reducers, which are functions that update the state
+    reducers: {
+        // Define a 'toggleDarkMode' reducer function that takes 'state' and 'action' as arguments
+        toggleDarkMode(state, action) {
+            // Invert the current value of 'state' to toggle between dark and light mode
+            return !state;
         }
     }
 })
 
+// Export the 'toggleDarkMode' action creator, which allows you to dispatch the 'toggleDarkMode' action
 export const { toggleDarkMode } = darkMode.actions;
+
+// Export the 'darkMode' reducer, which handles state updates based on dispatched actions
 export default darkMode.reducer;
